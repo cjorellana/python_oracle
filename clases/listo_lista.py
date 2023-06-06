0ed29722e94d11e4859794cdc3513cae44e94848
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-06-06 13:44:27 -0600

    Create listo_lista.py

---
+++ clases/listo_lista.py
@@ -0,0 +1,18 @@
+from ora_db import OracleDatabase
+
+# lista
+lista = []
+
+# listar
+db = OracleDatabase('localhost', '1521', 'XE', 'hr', 'hr')
+result = db.listado_pais()
+for row in result:
+    lista.append(row)
+    #print(row)
+
+db.close()
+
+print(type(lista))
+
+for fila in lista:
+    print(fila[0])
